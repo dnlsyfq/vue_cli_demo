b92d56ea767d9bc6344f2cde8a681d6e7a5299d3
dnlsyfq <danialsyafiq@outlook.my>
2024-01-29 23:45:08 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>Method Properties</h1>
+  <h3>My handle is {{ name }}</h3>
+  <h3>The first number: {{ firstNumber }}</h3>
+  <h3>The second number: {{ secondNumber }}</h3>
+  <h3>Result: {{  multiply(firstNumber,secondNumber) }}</h3>
+  <section style="align:left">
+  <label for="firstN">First Number: </label>
+  <input id="firstN" type="number" placeholder="Enter first number" v-model="firstNumber">
+
+  <label for="secondN">Second Number: </label>
+  <input id="secondN" type="number" placeholder="Enter second number" v-model="secondNumber">
+
+
+  </section>
+  <footer>Year {{ year() }}</footer>
 </template>
 
 <script>
@@ -8,8 +23,28 @@
 
 export default {
   name: 'App',
+  data(){
+    return {
+      name:"dnlsyfq",
+      prefix:20,
+      suffix:24,
+      firstNumber:'',
+      secondNumber:''
+    }
+  },
   components: {
     // HelloWorld
+  },
+  methods: {
+    fullName(){
+      return 'dnlsyfq' ;
+    },
+    year(){
+      return this.prefix + '' + this.suffix;
+    },
+    multiply(firstNumber,secondNumber){
+      return firstNumber * secondNumber
+    }
   }
 }
 </script>
